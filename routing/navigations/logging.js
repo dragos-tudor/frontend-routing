4509c1daa07b0d60bad36d796303a46e5c795b44
Dragos Tudor <dragos.tudor@gmail.com>
2023-11-19 15:45:51 +0000

    refacroeL remove unused files.

--- routing/navigations/logging.js
+++
@@ -1,9 +0,0 @@
-import { getHtmlName } from "../../routing-html/mod.js";
-import { getRouteData } from "../../routing-routes/mod.js";
-import { logInfo } from "../../support-loggers/mod.js"
-
-export const logConsumer = (consumer) =>
-  logInfo(consumer, `Routing consumer updated: ${getHtmlName(consumer)}`)
-
-export const logRoute = (route) =>
-  logInfo(route, `Route to ${getRouteData(route)}`)
