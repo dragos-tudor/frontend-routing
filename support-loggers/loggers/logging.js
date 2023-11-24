35c1793882883c94c8f44b5bde9c1a7cbed21342
Dragos Tudor <dragos.tudor@gmail.com>
2023-11-24 17:25:04 +0000

    refactor: normalize log funcs.

--- support-loggers/loggers/logging.js
+++ support-loggers/loggers/logging.js
@@ -3,6 +3,6 @@ import { isLogEnabled } from "./verifying.js"
 const LibraryName = "routing"
 const LogHeader = "[routing]"
 
-export const logError = (elem, ...args) => (isLogEnabled(elem, LibraryName) && console.error(LogHeader, ...args), args[0])
+export const logError = (elem, ...args) => isLogEnabled(elem, LibraryName) && console.error(LogHeader, ...args)
 
 export const logInfo = (elem, ...args) => isLogEnabled(elem, LibraryName) && console.info(LogHeader, ...args)
