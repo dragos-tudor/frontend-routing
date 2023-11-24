05f8397b2dab1cf62c06a6b07ca0a6ead9ffdd3a
Dragos Tudor <dragos.tudor@gmail.com>
2023-11-24 17:23:22 +0000

    fix: correct findind index route.

--- routing-routes/route-siblings/finding.js
+++ routing-routes/route-siblings/finding.js
@@ -1,5 +1,4 @@
 import { getHtmlChildren, getHtmlParentElement } from "../../routing-html/mod.js"
-import { isEmptyUrl } from "../../routing-urls/mod.js"
 import { getRouteData } from "../route-data/getting.js"
 import { isRoute, isMatchedRoute } from "../routes/verifying.js"
 
@@ -7,9 +6,8 @@ const findIndexRoute = (elems) =>
   elems.find(elem => getRouteData(elem).index)
 
 export const findSiblingRoute = (urlPart) => (elems) =>
-  !isEmptyUrl(urlPart)?
-    elems.find(isMatchedRoute(urlPart)):
-    findIndexRoute(elems)
+  elems.find(isMatchedRoute(urlPart)) ||
+  findIndexRoute(elems)
 
 export const findSiblingRoutes = (elem) =>
   getHtmlChildren(getHtmlParentElement(elem))
--- routing-routes/routes/finding.test.jsx
+++ routing-routes/routes/finding.test.jsx
@@ -6,47 +6,28 @@ import { findRoute } from "./finding.js"
 
 Deno.test("use routes => find route", async (t) => {
 
-  await t.step("non-matching url => find route => nothing", () => {
-    eq(findRoute(render(<a></a>), "/"), undefined)
-    eq(findRoute(render(<route __routeData={createRouteData("a")}></route>), "/"), undefined)
-  })
-
   await t.step("matching url => find route => route with matched path", () => {
-    eq(findRoute(render(<route __routeData={createRouteData("a")}></route>), "a").__routeData, createRouteData( "a"))
-    eq(findRoute(render(<route __routeData={createRouteData("a/b")}></route>), "a/b").__routeData, createRouteData( "a/b"))
-    eq(findRoute(render(<route __routeData={createRouteData("a")}></route>), "a/b").__routeData, createRouteData( "a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("a")}></route></a>), "a").__routeData, createRouteData("a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("a/b")}></route></a>), "a/b").__routeData, createRouteData("a/b"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("a")}></route></a>), "a/b").__routeData, createRouteData("a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("b")}></route><route __routeData={createRouteData("a")}></route></a>), "a").__routeData, createRouteData("a"))
-
     eq(findRoute(render(<route __routeData={createRouteData("/a")}></route>), "/a").__routeData, createRouteData( "/a"))
     eq(findRoute(render(<route __routeData={createRouteData("/a/b")}></route>), "/a/b").__routeData, createRouteData( "/a/b"))
     eq(findRoute(render(<route __routeData={createRouteData("/a")}></route>), "/a/b").__routeData, createRouteData( "/a"))
     eq(findRoute(render(<route __routeData={createRouteData("/a")}></route>), "/a/b").__routeData, createRouteData( "/a"))
+
     eq(findRoute(render(<a><route __routeData={createRouteData("/a")}></route></a>), "/a").__routeData, createRouteData("/a"))
     eq(findRoute(render(<a><route __routeData={createRouteData("/a")}></route></a>), "/a/b").__routeData, createRouteData("/a"))
+
     eq(findRoute(render(<a><route __routeData={createRouteData("/b")}></route><route __routeData={createRouteData("/a")}></route></a>), "/a").__routeData, createRouteData("/a"))
-
     eq(findRoute(render(<a><route __routeData={createRouteData("/a")}></route><route __routeData={createIndexRoute("/b")}></route></a>), "/a").__routeData, createRouteData("/a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("/")}></route></a>), "/a").__routeData, createRouteData("/"))
   })
 
-  await t.step("rooted url => find route => root route", () => {
-    eq(findRoute(render(<route __routeData={createRouteData("/")}></route>), "/").__routeData, createRouteData("/"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("/")}></route></a>), "/").__routeData, createRouteData("/"))
-    eq(findRoute(render(<route __routeData={createRouteData("/")}></route>), "/a").__routeData, createRouteData("/"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("/")}></route></a>), "/a").__routeData, createRouteData("/"))
+  await t.step("non-matching url => find route => nothing", () => {
+    eq(findRoute(render(<a></a>), "/"), undefined)
+    eq(findRoute(render(<route __routeData={createRouteData("/a")}></route>), "/b"), undefined)
   })
 
-  await t.step("no url => find route => index route", () => {
-    eq(findRoute(render(<route __routeData={createIndexRoute("a")}></route>), "").__routeData, createIndexRoute("a"))
-    eq(findRoute(render(<a><route __routeData={createIndexRoute("a")}></route></a>), "").__routeData, createIndexRoute("a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("b")}></route><route __routeData={createIndexRoute("a")}></route></a>), "").__routeData, createIndexRoute("a"))
-
-    eq(findRoute(render(<route __routeData={createIndexRoute("/a")}></route>), "").__routeData, createIndexRoute("/a"))
-    eq(findRoute(render(<a><route __routeData={createIndexRoute("/a")}></route></a>), "").__routeData, createIndexRoute("/a"))
-    eq(findRoute(render(<a><route __routeData={createRouteData("/b")}></route><route __routeData={createIndexRoute("/a")}></route></a>), "").__routeData, createIndexRoute("/a"))
+  await t.step("non-matching url and index route => find route => index route", () => {
+    eq(findRoute(render(<route __routeData={createIndexRoute("/a")}></route>), "/").__routeData, createIndexRoute("/a"))
+    eq(findRoute(render(<a><route __routeData={createIndexRoute("/a")}></route></a>), "/x").__routeData, createIndexRoute("/a"))
+    eq(findRoute(render(<a><route __routeData={createRouteData("/b")}></route><route __routeData={createIndexRoute("/a")}></route></a>), "/x").__routeData, createIndexRoute("/a"))
   })
 
 })
